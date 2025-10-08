e82db3eaa7338ed8e6603e0ba8aee727271fb6c4
Melvin issac <melvinrosario8248@gmail.com>
2025-10-08 13:21:23 +0530

    aws

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./model/userSchema");
+require('dotenv').config()
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log("mongo db connected successfully")
+})
+db.on("error", (err) => {
+    console.log("mongo db not connected", err)
+})
+
+
+
+app.get('/', (req, res) => {
+    res.json({ status: 200, message: "server is runing..." })
+})
+
+// CREATE
+app.post("/users", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// READ
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// UPDATE
+app.put("/users/:id", async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// DELETE
+app.delete("/users/:id", async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.json({ message: "User deleted" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Start Server
+app.listen(5000, () => console.log("🚀 Server running on port 5000"));
